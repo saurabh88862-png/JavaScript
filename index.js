f5b1e9b2783c3d39f47cdd53ba80fc991d34c476
lakshmi narayan ray <saurabh88862@gmail.com>
2026-01-30 23:43:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let h1 =document.querySelector('h1')
+let time= new Date().toLocaleTimeString()
+setInterval(() =>{
+    let time =new Date().toLocaleTimeString()
+    h1.innerText = time
+})
+
+
+
+
+
+
+
+
+
